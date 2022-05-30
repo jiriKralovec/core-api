8619eecbdcc3899498aa2cd1c4aacac31b2773d2
HQ106347 <jiri@solar.dk>
2022-05-30 11:49:28 +0200

    Added solution

---
+++ coreapi/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreapi.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var list = new List<string>();
+            var cookies = Request.Cookies;
+            foreach (var cookie in cookies)
+            {
+                list.Add($"{cookie.Key}:{cookie.Value}");
+            }
+            list.Add("foo");
+            return list;
+        }
+    }
+}
